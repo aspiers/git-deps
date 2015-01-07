350ad70b5b1dca7962f37a29327b17bf84365ae3
Adam Spiers <git@adamspiers.org>
2015-01-07 22:59:47 +0000

    make tip global for debugging

--- html/js/git-deps-graph.js
+++ html/js/git-deps-graph.js
@@ -10,7 +10,7 @@ var d3cola = cola.d3adaptor()
     .avoidOverlaps(true)
     .size([WIDTH, HEIGHT]);
 
-var svg, fg, node, path;
+var svg, fg, node, path, tip;
 
 jQuery(function () {
     draw_graph();
@@ -72,7 +72,7 @@ function draw_graph () {
 
 function draw_nodes (fg, node) {
     // Initialize tooltip
-    var tip = d3.tip().attr('class', 'd3-tip').html(tip_html);
+    tip = d3.tip().attr('class', 'd3-tip').html(tip_html);
     fg.call(tip);
     // Hide tooltip when we start dragging
     d3cola.drag().on('dragstart', tip.hide);
