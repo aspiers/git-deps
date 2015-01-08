d69fad2ce8f684e966a2f5433f73b6fa1a846525
Adam Spiers <git@adamspiers.org>
2015-01-08 18:47:01 +0000

    split noty helpers into separate file

--- html/js/git-deps-graph.js
+++ html/js/git-deps-graph.js
@@ -175,37 +175,6 @@ function init_svg() {
     fg = svg.append('g');
 }
 
-// Different noty types:
-// alert, success, error, warning, information, confirmation
-function noty_warn(text) {
-    notyfication('warning', text);
-}
-
-function noty_info(text) {
-    notyfication('information', text);
-}
-
-function noty_debug(text) {
-    notyfication('information', text);
-}
-
-// Haha, did you see what I did here?
-function notyfication(type, text) {
-    var n = noty({
-        text: text,
-        type: type,
-        layout: 'topRight',
-        theme: 'relax',
-        timeout: 30000,  // ms
-        animation: {
-            open: 'animated bounceInUp',   // Animate.css class names
-            close: 'animated bounceOutUp', // Animate.css class names
-            easing: 'swing',               // unavailable - no need
-            speed: 500                     // unavailable - no need
-        }
-    });
-}
-
 function draw_graph(commitish) {
     d3.json("deps.json/" + commitish, function (error, data) {
         var new_data = add_data(data);
