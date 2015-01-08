e749e283f9c1691ef7603e785bdd14c393f8e2ea
Adam Spiers <git@adamspiers.org>
2015-01-08 16:32:46 +0000

    split marker arrow definition into separate function

--- html/js/git-deps-graph.js
+++ html/js/git-deps-graph.js
@@ -161,17 +161,7 @@ function draw_graph(commitish) {
             .symmetricDiffLinkLengths(30);
             //.jaccardLinkLengths(100);
 
-        // define arrow markers for graph links
-        fg.append('svg:defs').append('svg:marker')
-            .attr('id', 'end-arrow')
-            .attr('viewBox', '0 -5 10 10')
-            .attr('refX', 6)
-            .attr('markerWidth', 8)
-            .attr('markerHeight', 8)
-            .attr('orient', 'auto')
-          .append('svg:path')
-            .attr('d', 'M0,-5L10,0L0,5')
-            .attr('fill', '#000');
+        define_arrow_markers(fg);
 
         path = fg.selectAll(".link")
             .data(links, function (d) {
@@ -190,6 +180,20 @@ function draw_graph(commitish) {
 
         draw_nodes(fg, node);
     });
+}
+
+function define_arrow_markers(fg) {
+    // define arrow markers for graph links
+    fg.append('svg:defs').append('svg:marker')
+        .attr('id', 'end-arrow')
+        .attr('viewBox', '0 -5 10 10')
+        .attr('refX', 6)
+        .attr('markerWidth', 8)
+        .attr('markerHeight', 8)
+        .attr('orient', 'auto')
+      .append('svg:path')
+        .attr('d', 'M0,-5L10,0L0,5')
+        .attr('fill', '#000');
 }
 
 function draw_nodes(fg, node) {
