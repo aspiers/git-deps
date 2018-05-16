02e79d29f71feb795779d97825644ad4172598ad
Adam Spiers <git@adamspiers.org>
2018-05-16 16:39:57 +0100

    try using setuptools-markdown to fix the long_description

--- setup.py
+++ setup.py
@@ -15,8 +15,15 @@ from setuptools import setup
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
-          use_pyscaffold=True)
+    setup(
+        setup_requires=[
+            'six',
+            'pyscaffold>=2.5a0,<2.6a0',
+            'setuptools-markdown',
+        ] + sphinx,
+        long_description_markdown_filename='README.md',
+        use_pyscaffold=True
+    )
 
 
 if __name__ == "__main__":
